0d45d2d78f772c0f00122b0b3615b6b2df7855f3
Federico Rossendy <fede07@users.noreply.github.com>
2023-10-15 16:54:15 -0300

    Gui

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+from tkinter import *
+from turtle import color, title
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -8,6 +9,7 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+CHECKMARK = "✅"
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
@@ -16,3 +18,28 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+title_label = Label(text="timer", bg=YELLOW, font=(FONT_NAME, 32, "bold"), fg=GREEN)
+title_label.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start")
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset")
+reset_button.grid(column=2, row=2)
+
+checkmark_label = Label(text="")
+checkmark_label.grid(column=1, row=3)
+
+
+window.mainloop()
